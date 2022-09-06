61f5a23799592c4f86f14be2ddd0ba5a138dea3d
al lisyatnikov <a.lisyatnikov@gmail.com>
2022-09-06 16:43:17 +0300

    making idea in main file

--- Pioneer_vs_Boogeymans.py
+++ Pioneer_vs_Boogeymans.py
@@ -1,14 +1,30 @@
 def main():
-    hp = 10
+    boar_create()
     the_end_of_game = False
     while not the_end_of_game:
         print('enemy is alive', hp, 'HP')
         damage = int(input())
-        hp -= damage
+        boar_get_damage(damage)
 
-        if hp <= 0:
+        if not boar_is_alive():
             the_end_of_game = True
             print('All enemies are dead')
+
+
+def boar_create():
+    global hp
+    hp = 10
+
+
+def boar_is_alive():
+    return hp > 0
+
+
+def boar_get_damage(damage):
+    global hp
+    hp -= damage
+    if hp < 0:
+        hp = 0
 
 
 main()

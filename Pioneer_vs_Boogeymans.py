edf711fd3bad38741efe0989b0a96614edb10c52
al lisyatnikov <a.lisyatnikov@gmail.com>
2022-09-07 13:37:17 +0300

    fixed

--- Pioneer_vs_Boogeymans.py
+++ Pioneer_vs_Boogeymans.py
@@ -1,13 +1,21 @@
 def main():
-    enemy = Boar('Pops')
+    enemy_list = [Boar('Pops'), Boar('Frank'), Hornet('Zuo')]
 
     the_end_of_game = False
     while not the_end_of_game:
+        enemy = enemy_list[0]
         enemy.health_status()
         damage = int(input())
         enemy.get_damage(damage)
 
         if not enemy.is_alive():
+            enemy_list.pop(0) # Eresing the dead Enemy in the enemy list.
+            # But much better do POP to the end of list
+            # You have to change fight logic with multiple enemies
+
+
+
+        if not enemy_list: # Checking is anyone enemy is alive
             the_end_of_game = True
             print('All enemies are dead')
 
@@ -35,6 +43,7 @@ class Boar:
 
 class Hornet:
     def __init__(self, enemy_id):
+        self.enemy_id = enemy_id
         self.hp = 1
 
     def is_alive(self):
@@ -46,7 +55,7 @@ class Hornet:
             self.hp = 0
 
     def health_status(self):
-        print('enemy is alive', self.hp, 'HP')
+        print('enemy hornet', self.enemy_id, ' is alive', self.hp, 'HP')
 
     def dying_cry(self):
         print('BzzzzzWooo!!?')
